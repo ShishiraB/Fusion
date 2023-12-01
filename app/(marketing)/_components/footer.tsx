52bd757f04496cf931537311a8489d25ba52ed03
Shishira Bhat <shishira.21ai050@sode-edu.in>
2023-12-01 06:21:08 +0530

    button changes

--- app/(marketing)/_components/footer.tsx
+++ app/(marketing)/_components/footer.tsx
@@ -1,17 +1,19 @@
+'use client';
+
 import { Button } from "@/components/ui/button"
 
 import { Logo } from "./logo"
 
 export const Footer = () => {
+  const handleClick = () => {
+    window.open('https://github.com/ShishiraB/clipnote', '_blank');
+  };
   return (
     <div className="flex items-center w-full p-6 bg-background z-50 dark:bg-[#1F1F1F]">
       <Logo />
       <div className="md:ml-auto w-full justify-between md:justify-end flex items-center gap-x-2 text-muted-foreground">
-        <Button variant="ghost" size="sm">
-          Privacy Policy
-        </Button>
-        <Button variant="ghost" size="sm">
-          Terms & Conditions
+        <Button variant="ghost" size="sm" onClick={handleClick}>
+          GitHub
         </Button>
       </div>
     </div>
